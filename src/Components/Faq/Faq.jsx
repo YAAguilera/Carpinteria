a65b15fe1b741d2f182dbc0b85852119e2671347
Gxspxr <sernagas1234@gmail.com>
2023-07-25 20:50:03 -0300

    a

--- src/Components/Faq/Faq.jsx
+++ src/Components/Faq/Faq.jsx
@@ -7,7 +7,7 @@ const Faq = ({question, answer, isDrop, onDrop}) => {
     <article className={`${isDrop.isOpen ? 'h-auto' : 'h-auto'} transition-all duration-200 bg-gray-200 border-2 px-4 shadow-md rounded-3xl xxl:w-[30em] xl:w-[30em] lg:w-[30em] md:w-[30em] sm:w-[30em] xs:w-[20em]  pb-2 cursor-pointer`} onClick={onDrop}>
       <div className="flex flex-row items-center">
         <RiArrowDropRightLine className={`text-5xl text-secondary cursor-pointer ${isDrop.isOpen && 'rotate-90'} transition-all`} />
-        <p className="flex text-base-light text-lg font-medium">{question}</p>
+        <p className="flex text-base-light text-lg font-semibold">{question}</p>
     
       </div>
         <p className={`text-base-light/80 block ${!isDrop.isOpen ? ' hidden' : 'block' } transition-opacity`}>
